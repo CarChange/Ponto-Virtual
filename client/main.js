efb455db3e8dfa1c3a2d9ed499711f6e8a4d3ff8
danCastro <dcostadcastro@gmail.com>
2017-04-25 17:45:59 -0300

    import da pasta ao invés de cada arquivo

--- client/main.js
+++ client/main.js
@@ -9,7 +9,8 @@ import '../imports/ui/components/template/main-menu.js';
 import '../imports/ui/components/template/modernizr-2.6.2-respond-1.1.0.min.js';
 import '../imports/ui/components/template/template.js';
 import '../imports/ui/components/template/leaflet.js';
-import '../imports/ui/stylesheets/bootstrap-theme.css';
+
+/*import '../imports/ui/stylesheets/bootstrap-theme.css';
 import '../imports/ui/stylesheets/coming-soon-social.css';
 import '../imports/ui/stylesheets/icomoon-social.css';
 import '../imports/ui/stylesheets/leaflet.css';
@@ -17,4 +18,6 @@ import '../imports/ui/stylesheets/leaflet.ie.css';
 import '../imports/ui/stylesheets/main.css';
 import '../imports/ui/stylesheets/main-green.css';
 import '../imports/ui/stylesheets/main-orange.css';
-import '../imports/ui/stylesheets/main-red.css';
+import '../imports/ui/stylesheets/main-red.css';*/
+
+import '../imports/ui/stylesheets';
